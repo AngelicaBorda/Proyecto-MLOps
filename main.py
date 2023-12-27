7b8eb1cb579d20fe596871c2cd2e333e5a4d322d
AngelicaBorda <angeborda@gmail.com>
2023-12-27 16:27:10 -0300

    cambios

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ def PlayTimeGenre(genero: str = None) -> dict:
     # Obtener el año con más horas jugadas
     año_max_horas = df_suma_horas.idxmax()
     
-    result = {"Año de lanzamiento con más horas jugadas para el género " + genero: año_max_horas}
-
+    result = {"Año de lanzamiento con más horas jugadas para el género {}".format(genero): año_max_horas}
+      
     return result 
 
 
